59e38bd143354f0e83ee5a7e870bb80ad987951c
TebbaaX <>
2023-03-18 14:47:19 +0100

    Major updates...

---
+++ src/components/main.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+export default class Main extends React.Component {
+    constructor(){
+        super();
+    }
+
+render() {
+
+    return (
+        
+<div class="hero-section pt-8 lg:pt-8 sm:pt-32">
+<section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-24 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-sans tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">👋 Hi! i'm Adnane Tebbaa </h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 font-sans fon">
+            Full-stack developer with Cyber Security background, specializing in building secure applications with a range of tech and programming languages. Experienced in user interface, server-side logic, and database management.
+            With a deep understanding of cybersecurity best practices. 
+            delivering high-quality and secure solutions for clients.</p>
+            
+           <form action="mailto:tebbaa.adnane.dev@gmail.com" className="font-sans">
+    <button type="submit" value="mailto:tebbaa.adnane.dev@gmail.com" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 shadow-2xl">Contact</button>
+    <button class="btn btn-outline-primary">See resume</button>
+           </form>
+
+         </div>
+    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="images/ea036822.png" alt="mockup"></img>
+        </div>                
+    </div>
+</section>
+</div>
+
+    );
+}
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './components/header';
+import Main from './components/main';
+import Technologies from './components/Technologies';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+const navbar = ReactDOM.createRoot(document.getElementById("header"));
+navbar.render(<Header/>)
+
+const main = ReactDOM.createRoot(document.getElementById('main'));
+main.render(<Main/>)
+
+const maintwo = ReactDOM.createRoot(document.getElementById('maintwo'));
+maintwo.render(<Technologies/>)
+
+
 reportWebVitals();
