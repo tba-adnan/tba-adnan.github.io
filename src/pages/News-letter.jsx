b4bd1b1daa95e9b6c9abf97807c8d592d6296d01
TebbaaX <>
2023-04-10 01:10:55 +0000

    News Letter UI fix, Bug fix & more...

--- src/pages/News-letter.jsx
+++ src/pages/News-letter.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
+import Footer from '../components/footer';
 import YouTubePlayer from '../components/ytplayer';
 
 function Newsletter() {
   return (
-    // <div className="relative pt-[70px] flex justify-center px-14">
-    //   <div className="relative flex justify-center pt-[120px]">
-    //     <iframe
-    //       className="shadow-lg shadow-inner rounded-xl"
-    //       src="https://tebbaa.substack.com/embed"
-    //       width="480"
-    //       height="320"
-    //       style={{ border: '1px solid #EEE', background: 'white' }}
-    //       frameborder="0"
-    //       scrolling="no"
-    //       allowtransparency="true"
-    //     ></iframe>
-    //   </div>
-    // </div>
+  
+
+<div className='pb-0 pt-9 '>
+<div className="flex justify-center items-center h-screen bg-blue-500">
+  <div className="max-w-none w-[650px] w-full mx-2">
+    <iframe
+      className="shadow-lg shadow-inner rounded-2xl"
+      src="https://tebbaa.substack.com/embed"
+      width="99%"
+      height="320"
+      style={{ border: '1px solid #EEE', background: '' }}
+      frameborder="0"
+      scrolling="no"
+      allowtransparency="true"
+    ></iframe>
+     <div className="pt-7">
+            <a href="/" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-red-500 rounded-xl group">
+<span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-red-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+<span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+</span>
+<span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-red-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+<span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white font-sans">Back</span>
+</a>
+            </div>
+  </div>
+
+</div>
+<div className=''> <Footer/></div>
+
+</div>
+
+
+
 
 
  
-  <div></div>
+
+
+
+
+
 
 
 
