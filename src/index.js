cba4fbd0bf17ffd0f58218a1049f351d8863d1c1
TebbaaX <>
2023-04-11 22:02:12 +0000

    Switch to hash router.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-
+// 
 import Layout from './pages/Layout';
 import Notfound from './pages/Notfound'
 import Home from './pages/Home';
 import About from './pages/About';
 import Newsletter from './pages/News-letter';
 
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 export default function App() {
   return (
-    <BrowserRouter basename="">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={< About/>} />
-          <Route path="error" element={<Notfound />} />
+    <Router basename="">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
           <Route path="news-letter" element={<Newsletter />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
   
