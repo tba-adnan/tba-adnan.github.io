b52414c7c0b476bcb93d8baee0679dbec2c020db
TebbaaX <>
2023-04-11 22:39:19 +0000

    Revert.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// 
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+
+
+import Layout from './pages/Layout';
+import Notfound from './pages/Notfound'
 import Home from './pages/Home';
 import About from './pages/About';
 import Newsletter from './pages/News-letter';
-import NotFound from './pages/NotFound';
-
-
 
 
 export default function App() {
   return (
-    <BrowserRouter basename="/">
-      <Layout>
-        <Switch>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/news-letter" element={<Newsletter />} />
-          <Route path="*" element={<NotFound />} />
-        </Switch>
-      </Layout>
+    <BrowserRouter basename="">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={< About/>} />
+          <Route path="error" element={<Notfound />} />
+          <Route path="news-letter" element={<Newsletter />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
