909400a5b5bc480be0fc1b09c05a1d970575d980
TebbaaX <>
2023-04-11 00:03:11 +0000

    Routing bug fix.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
-// import App from './App';
-import Header from './components/header';
-import Footer from './components/footer';
-import Main from './components/main';
-import Maintwo from './components/main_two';
-import Technologies from './components/Technologies';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -19,24 +14,21 @@ import Newsletter from './pages/News-letter';
 
 
 export default function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           {/* <Route path="blogs" element={<Home />} /> */}
           <Route path="about" element={< About/>} />
-            <Route path="error" element={<Notfound />} />
-            <Route path="news-letter" element={<Newsletter />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+          <Route path="error" element={<Notfound />} />
+          <Route path="news-letter" element={<Newsletter />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
   
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<App />);
-
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 reportWebVitals();
