2b782e82ae4162577fe02c0da452812912fcf3d5
TebbaaX <>
2023-04-11 22:19:05 +0000

    Switched to public env.URL.

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 // 
-import Layout from './pages/Layout';
-import Notfound from './pages/Notfound'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Newsletter from './pages/News-letter';
+import Newsletter from './pages/Newsletter';
+import NotFound from './pages/NotFound';
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 
 export default function App() {
   return (
-    <Router basename="">
+    <Router basename={process.env.PUBLIC_URL}>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="news-letter" element={<Newsletter />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/news-letter" component={Newsletter} />
+          <Route component={NotFound} />
+        </Switch>
       </Layout>
     </Router>
   );
