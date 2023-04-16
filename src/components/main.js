cbab687664dcc5f19982ebd7ea4850440532091b
TebbaaX <58398485+TebbaaX@users.noreply.github.com>
2023-04-16 14:21:20 +0000

    added redirect page.

--- src/components/main.js
+++ src/components/main.js
@@ -24,7 +24,7 @@ render() {
             <form action="mailto:tebbaa.adnane.dev@gmail.com" className="font-sans">
             <button type="submit" value="mailto:tebbaa.adnane.dev@gmail.com" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 shadow-2xl">Get In Touch</button>
             </form>
-            <form action="https://www.linkedin.com/in/adnane-tba/">
+            <form action="/redirect">
             <button class="btn btn-outline-primary" href="google.com">See resume</button>
             </form>
             </div>
